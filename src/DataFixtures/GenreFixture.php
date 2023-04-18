bc48d539d72300eefdb39a2bd380478bd9552014
Heaven31415 <heaven31415@gmail.com>
2023-04-18 15:17:29 +0200

    Add groups to GenreFixture

--- src/DataFixtures/GenreFixture.php
+++ src/DataFixtures/GenreFixture.php
@@ -4,10 +4,16 @@ namespace App\DataFixtures;
 
 use App\Entity\Genre;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class GenreFixture extends Fixture
+class GenreFixture extends Fixture implements FixtureGroupInterface
 {
+    public static function getGroups(): array
+    {
+        return ['prod', 'test'];
+    }
+
     public function load(ObjectManager $manager): void
     {
         $genreNames = array(
